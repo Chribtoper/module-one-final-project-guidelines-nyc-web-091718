0a0cb2c0a6f6c444f8a67f22e6d8c0d4638d7863
mx@E <mglxp5@gmail.com>
2018-10-03 15:44:31 -0400

    updated game#do_command

--- app/models/game.rb
+++ app/models/game.rb
@@ -13,14 +13,41 @@ class Game
   end
 
   def print_commands
+    # exit
+    # help
+    # create npc custom
+    # create npc random
+  end
+
+  def create_npc_custom
+    puts "Enter [name][race][age]"
+    input = gets.chomp
+    split_in = input.split
+    
+  end
+
+  def create_npc_random
+
   end
 
   def do_command(cmd)
-    case cmd
+    split_cmd = cmd.split
+    case split_cmd[0]
     when "exit"
       @running = false
     when "help"
       print_commands
+    when "create"
+      case split_cmd[1]
+      when "npc"
+        case split_cmd[2]
+        when "custom"
+          create_npc_custom
+        when "random"
+          create_npc_random
+        end
+      #when "town"
+      end
     else
       puts "#{cmd} is not a valid command"
     end
